d75a3f32dc825b0e24759dd063679f683656a57b
thekindsir <ckyser2@gmail.com>
2016-12-05 23:43:08 -0600

    Add files via upload

--- src/main/java/com/example/chris/studygroup/Group.java
+++ src/main/java/com/example/chris/studygroup/Group.java
@@ -3,13 +3,25 @@ package com.example.chris.studygroup;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class Group extends AppCompatActivity implements View.OnClickListener {
 
 
-
     FloatingActionButton butSearch, butHome, butAdd, butPro, butCal;
+    Button butCreate;
+    EditText txtSub, txtGrp;
+    Spinner spinSub;
+    SpinnerAdapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +34,60 @@ public class Group extends AppCompatActivity implements View.OnClickListener {
         butPro = (FloatingActionButton) findViewById(R.id.butPro);
         butCal = (FloatingActionButton) findViewById(R.id.butCal);
 
+        butCreate = (Button) findViewById(R.id.butCreate);
+
+        txtGrp = (EditText) findViewById(R.id.txtGrp);
+        txtSub = (EditText) findViewById(R.id.txtSub);
+
+        spinSub =(Spinner) findViewById(R.id.spinSub);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,MainActivity.subjects);
+
         butHome.setOnClickListener(this);
         butSearch.setOnClickListener(this);
         butAdd.setOnClickListener(this);
         butPro.setOnClickListener(this);
         butCal.setOnClickListener(this);
+
+        butCreate.setOnClickListener(this);
+
+        spinSub.setAdapter(adapter);
+
+
+
     }
 
     @Override
     public void onClick(View v) {
-        if( v ==butHome){
+        if (v == butHome) {
             finish();
-        }
-        else if( v==butSearch){
+        } else if (v == butSearch) {
             startActivity(MainActivity.Search);
             finish();
-        }
-        else if(v==butAdd){
-        }
-        else if ( v==butPro){
+        } else if (v == butAdd) {
+        } else if (v == butPro) {
             startActivity(MainActivity.Profile);
             finish();
-        }
-        else if (v == butCal){
+        } else if (v == butCal) {
             startActivity(MainActivity.Calendar);
             finish();
+        } else if (v == butCreate) {
+
+            if (txtGrp.getText().length()==0) {
+                txtGrp.setError("Group Name Cannot be blank");
+            } else {
+                MainActivity.user.createGroup(txtGrp.getText().toString(), spinSub.getSelectedItem().toString());
+                MainActivity.currentGrp=MainActivity.tree.getGroup(txtGrp.getText().toString());
+
+            }
+
+
+
         }
+
+
     }
+
 }
+
+
--- src/main/java/com/example/chris/studygroup/MainActivity.java
+++ src/main/java/com/example/chris/studygroup/MainActivity.java
@@ -6,12 +6,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    static Intent Login,Group,Calendar,Profile,Search;
+    public static Intent Login,Group,Calendar,Profile,Search;
 
-    static GroupTree test;
+    public static GroupTree tree;//main tree
+    public static UserProfile user;
+    public static String[] subjects;
+    public static LinkedList currentGrp;
+
+    Button butFind;
+    EditText txtSearch;
+    TextView itemOne,itemTwo,itemThree,itemFour,itemFive,itemSix;
+    TextView[] groupList;
+    GenData[] groups;
 
     FloatingActionButton butSearch, butHome, butAdd, butPro, butCal;
 
@@ -20,17 +32,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        groupList=new TextView[6];
+
         butHome = (FloatingActionButton) findViewById(R.id.butHome);
         butSearch = (FloatingActionButton) findViewById(R.id.butSearch);
         butAdd = (FloatingActionButton) findViewById(R.id.butAdd);
         butPro = (FloatingActionButton) findViewById(R.id.butPro);
         butCal = (FloatingActionButton) findViewById(R.id.butCal);
 
+        butFind = (Button) findViewById(R.id.butFind);
+        txtSearch = (EditText) findViewById(R.id.txtSearch);
+
+        itemOne =(TextView) findViewById(R.id.itemOne);
+        itemTwo =(TextView) findViewById(R.id.itemTwo);
+        itemThree =(TextView) findViewById(R.id.itemThree);
+        itemFour =(TextView) findViewById(R.id.itemFour);
+        itemFive =(TextView) findViewById(R.id.itemFive);
+        itemSix =(TextView) findViewById(R.id.itemSix);
+
         butHome.setOnClickListener(this);
         butSearch.setOnClickListener(this);
         butAdd.setOnClickListener(this);
         butPro.setOnClickListener(this);
         butCal.setOnClickListener(this);
+
+        butFind.setOnClickListener(this);
+
+        groupList[0]=itemOne;
+        groupList[1]=itemTwo;
+        groupList[2]=itemThree;
+        groupList[3]=itemFour;
+        groupList[4]=itemFive;
+        groupList[5]=itemSix;
+
+        groups= new LinkedList[]{};
+        user= new UserProfile("guest","guest@siu.edu","password"); ///will change to get user from login screen.
+
+        tree = new GroupTree();
+        subjects= new String[]{"ACCOUNTING", "ADVANCED ENERGY & FUELS MGMT", "AEROSPACE STUDIES", "AFRICANA STUDIES", "AGRIBUSINESS ECONOMICS",
+                "ARGRICULTURAL SCIENCES", "AGRICULTURAL SYSTEMS & EDUCAT.", "AGRICULTURE", "ALLIED HEALTH", "ANIMAL SCIENCE", "ANTHROPOLOGY",
+                "ARCHITECTURAL STUDIES", "ARMY MILITARY SCIENCE", "ART AND DESIGN", "AUTOMOTIVE TECHNOLOGY", "AVIATION FLIGHT", "AVIATION MANAGEMENT", "AVIATION TECHNOLOGIES",
+                "BEHAVIOR ANALYSIS AND THERAPY", "BIOCHEMISTRY", "BIOLOGY", "BIOMEDICAL ENGINEERING", "BUSINESS", "BUSINESS ADMINISTRATION", "CHEMISTRY AND BIOCHEMISTRY", "CHINESE",
+                "CINEMA AND PHOTOGRAPHY", "CIVIL ENGINEERING", "CLASSICS", "COMMUNICATION DISORDERS & SCI.", "COMMUNICATION STUDIES",
+                "COMPUTER SCIENCE", "COUNSELING", "CRIMINOLOGY & CRIMINAL JUSTICE", "CROP, SOIL, ENVIRONMENTAL MGMT", "CURRICULUM AND INSTRUCTION",
+                "DENTAL HYGIENE", "EAST ASIA", "ECONOMICS", "EDU. ADMIN. & HIGHER EDUC.", "EDUCATION",
+                "ELECTRICAL & COMPUTER ENGINEERING", "ELECTRONIC SYSTEMS TECHNOLGIES", "ENGINEERING", "ENGINEERING TECHNOLOGY", "ENGLISH",
+                "ENVIRONMENTAL RESOURCES & POLICY", "FASHION DESIGN & MERCHANDISING", "FERMENTATION SCIENCE", "FINANCE", "FIRE SCIENCE MANAGEMENT", "FOOD & NUTRITION", "FOREIGN LANGUAGES",
+                "FORESTRY", "FRENCH", "GEOGRAPHY", "GEOLOGY", "GERMAN", "GERONTOLOGY", "HEALTH ADMINISTRATION", "HEALTH CARE MANAGEMENT", "HEALTH EDUCATION", "HEALTH INFORMATICS",
+                "HISTORY", "HISOTECHNOLOGY", "HORTICULTURE", "HOSPITALITY & TOURISM ADMINISTRATION", "HUMAN NUTRITION & DIETETICS",
+                "INDUSTRIAL MANAGEMENT & APPLIED ENGINEERING", "INFORMATION SYSTEMS & APPLIED TECHNOLOGY",
+                "JOURNALISM", "KINESIOLOGY", "LIBERAL ARTS", "LINGUISTICS", "MANAGEMENT", "MARKETING", "MASS COMMUNICATION & MEDIA ARTS",
+                "MATHEMATICS", "MECHANICAL ENGINEERING", "MEDICAL EDUCATION PREPARATION", "MICROBIOLOGY", "MINING ENGINEERING", "MOLECULAR BIOLOGY, MICRO BIOCHEMISTRY",
+                "MORTUARY SCIENCE & FUNERAL SERVICE", "MUSIC", "PARALEGAL STUDIES, LEGAL ASSISTANCE", "PHARMACOLOGY", "PHILOSOPHY",
+                "PHYSICAL THERAPIST ASSISTANT", "PHYSICIAN ASSISTANT", "PHYSICS", "PHYSIOLOGY", "PLANT BIOLOGY", "PLANT, SOIL, & AGRICULTURAL SYSTEMS", "POLITICAL SCIENCE",
+                "PSYCHOLOGY", "QUANTITATIVE METHODS", "RADIO, TV, AND DIGITAL MEDIA", "RADIOLOGIC SCIENCES", "RECREATION", "REHABILITATION", "SCHOOL OF MEDICINE", "SOCIAL WORK", "SOCIOLOGY", "SPANISH",
+                "SPECIAL EDUCATION", "TECHNICAL & RESOURCE MANAGEMENT", "THEATER", "UNIVERSITY HONORS", "WOMEN, GENDER, & SEX. STUDIES", "WORKFORCE EDUCATION. & DEVELOPMENT", "ZOOLOGY"};
+        tree.addSubArr(subjects);
+
+        updateList();
+
+
 
         Group = new Intent(this, Group.class);
         Calendar = new Intent(this, Calendar.class);
@@ -41,20 +103,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(Login);
 
 
-
 //-----------------------------------------------------------------------------------------------------------------------------------+
 //                Test for Linked List                                                                                               |
 //-----------------------------------------------------------------------------------------------------------------------------------+
 
-        test = new GroupTree();
 
-        GenData chris, demarco,colton, art, science, reading;
 
-        art= new LinkedList("Art");
-        science= new LinkedList("Science");
-        reading= new LinkedList("Reading");
 
-        GenData[]subjects = {art, science, reading};
+        GenData chris, demarco,colton;
+
+
+
 
         chris=new UserProfile("Chris","ckyser2@gmail.com","password");
 
@@ -63,14 +122,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         colton=new UserProfile("Colton","colton@gmail.com","password");
 
 
-        test.addSub(subjects);
-        System.out.println(test.toString());
-        chris.createGroup("group","Art");
-        test.addUser(chris,"group");
-        test.addUser(demarco,"group");
-        test.addUser(colton,"group");
-        System.out.println(test.toString());
-        GenData group =test.getGroup("group");
+
+        System.out.println(tree.toString());
+        chris.createGroup("group","Generic");
+        chris.createGroup("test","Generic");
+        tree.addUser(chris,"group");
+        tree.addUser(demarco,"group");
+        tree.addUser(colton,"group");
+        System.out.println(tree.toString());
+        GenData group =tree.getGroup("group");
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------+
+//                Test for Queue                                                                                             |
+//-----------------------------------------------------------------------------------------------------------------------------------+
+
+        Message a = new Message("test","this is a test meassg1e");
+        Message b = new Message("test","this is a test meassge2");
+
+        Message c = new Message("test","this is a test meassge3");
+        Message d = new Message("test","this is a test meassge4");
+        Message e = new Message("test","this is a test meassge5");
+        Message f = new Message("test","this is a test meassge6");
+        Message g = new Message("test","this is a test meassge7");
+        Message h = new Message("test","this is a test meassge8");
+        Message i = new Message("test","this is a test meassge9");
+
+        Queue q = new Queue(5);
+        q.push(a);
+        q.push(b);
+        q.push(c);
+        System.out.println(q.toString());
+        q.push(d);
+        q.push(e);
+        q.push(f);
+        System.out.println(q.toString());
+        q.push(g);
+        q.push(h);
+        q.push(i);
+
+        System.out.println(q.toString());
 
 
     }
@@ -92,8 +185,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if ( v==butPro){
             startActivity(Profile);
         }
-        else if (v == butCal){
+        else if (v == butCal) {
             startActivity(Calendar);
         }
+            else if (v == butFind){
+                updateList();
+            }
+        }
+    public void updateList(){
+        if(user.getGroups().getLength()!=0) groups=user.getGroups().toArr();
+
+
+        for(int x=0;x<groups.length&&x<5;x++){
+            groupList[x].setText(" ");
+            if(groups[x]!=null)groupList[x].setText(" "+groups[x].toString());
+
+        }
     }
+
 }
