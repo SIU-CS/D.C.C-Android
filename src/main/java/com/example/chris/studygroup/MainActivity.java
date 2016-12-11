84dd192866404123bf8ef606286ad8261f075e61
Jazhan <demarco.ewings@siu.edu>
2016-12-11 00:23:54 -0600

    Update MainActivity.java

--- src/main/java/com/example/chris/studygroup/MainActivity.java
+++ src/main/java/com/example/chris/studygroup/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.support.design.widget.FloatingActionButton;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,7 +18,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static String[] subjects;
     public static LinkedList currentGrp;
 
-    Button butFind;
+    Button butFind,remOne,remTwo,remThree,remFour,remFive;
+    Button[] remove;
     EditText txtSearch;
     TextView itemOne,itemTwo,itemThree,itemFour,itemFive,itemSix;
     TextView[] groupList;
@@ -33,7 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        groupList=new TextView[6];
+        remove = new Button[5];
+        groupList= new TextView[5];
 
         butHome = (FloatingActionButton) findViewById(R.id.butHome);
         butSearch = (FloatingActionButton) findViewById(R.id.butSearch);
@@ -41,15 +42,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butPro = (FloatingActionButton) findViewById(R.id.butPro);
         butCal = (FloatingActionButton) findViewById(R.id.butCal);
 
-        butFind = (Button) findViewById(R.id.butFind);
-        txtSearch = (EditText) findViewById(R.id.txtSearch);
+        butFind = (Button) findViewById(R.id.butFind0);
+        txtSearch = (EditText) findViewById(R.id.txtSearch0);
 
-        itemOne =(TextView) findViewById(R.id.itemOne);
-        itemTwo =(TextView) findViewById(R.id.itemTwo);
-        itemThree =(TextView) findViewById(R.id.itemThree);
-        itemFour =(TextView) findViewById(R.id.itemFour);
-        itemFive =(TextView) findViewById(R.id.itemFive);
-        itemSix =(TextView) findViewById(R.id.itemSix);
+        itemOne =(TextView) findViewById(R.id.itemOne0);
+        itemTwo =(TextView) findViewById(R.id.itemTwo0);
+        itemThree =(TextView) findViewById(R.id.itemThree0);
+        itemFour =(TextView) findViewById(R.id.itemFour0);
+        itemFive =(TextView) findViewById(R.id.itemFive0);
+
+        remOne = (Button) findViewById(R.id.remOne);
+        remTwo = (Button) findViewById(R.id.remtwo);
+        remThree = (Button) findViewById(R.id.remThree);
+        remFour = (Button) findViewById(R.id.remFour);
+        remFive = (Button) findViewById(R.id.remFive);
 
         butHome.setOnClickListener(this);
         butSearch.setOnClickListener(this);
@@ -59,17 +65,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         butFind.setOnClickListener(this);
 
+        remOne.setOnClickListener(this);
+        remTwo.setOnClickListener(this);
+        remThree.setOnClickListener(this);
+        remFour.setOnClickListener(this);
+        remFive.setOnClickListener(this);
+
+        remOne.setVisibility(View.INVISIBLE);
+        remTwo.setVisibility(View.INVISIBLE);
+        remThree.setVisibility(View.INVISIBLE);
+        remFour.setVisibility(View.INVISIBLE);
+        remFive.setVisibility(View.INVISIBLE);
+
         groupList[0]=itemOne;
         groupList[1]=itemTwo;
         groupList[2]=itemThree;
         groupList[3]=itemFour;
         groupList[4]=itemFive;
-        groupList[5]=itemSix;
+
+        remove[0]=remOne;
+        remove[1]=remTwo;
+        remove[2]=remThree;
+        remove[3]=remFour;
+        remove[4]=remFive;
+
 
         groups= new LinkedList[]{};
         user= new UserProfile("guest","guest@siu.edu","password"); ///will change to get user from login screen.
-//Is there any other way you can access this data besieds typing every subject the is offered //
-        // Code Review //
+
         tree = new GroupTree();
         subjects= new String[]{"ACCOUNTING", "ADVANCED ENERGY & FUELS MGMT", "AEROSPACE STUDIES", "AFRICANA STUDIES", "AGRIBUSINESS ECONOMICS",
                 "ARGRICULTURAL SCIENCES", "AGRICULTURAL SYSTEMS & EDUCAT.", "AGRICULTURE", "ALLIED HEALTH", "ANIMAL SCIENCE", "ANTHROPOLOGY",
@@ -91,7 +114,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 "SPECIAL EDUCATION", "TECHNICAL & RESOURCE MANAGEMENT", "THEATER", "UNIVERSITY HONORS", "WOMEN, GENDER, & SEX. STUDIES", "WORKFORCE EDUCATION. & DEVELOPMENT", "ZOOLOGY"};
         tree.addSubArr(subjects);
 
-        updateList();
 
 
 
@@ -102,6 +124,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Login = new Intent(this, LoginScreen.class);
         startActivity(Login);
+        updateList();
 
 
 //-----------------------------------------------------------------------------------------------------------------------------------+
@@ -111,26 +134,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-        GenData chris, demarco,colton;
+        GenData chris, demarco,colten;
 
-// Are these user profiles generice classes? //
-        //Code Review //
+
 
 
         chris=new UserProfile("Chris","ckyser2@gmail.com","password");
 
         demarco=new UserProfile("Demarco","demarco2@gmail.com","password");
 
-        colton=new UserProfile("Colton","colton@gmail.com","password");
+        colten=new UserProfile("Colten","colten@gmail.com","password");
 
 
 
         System.out.println(tree.toString());
-        chris.createGroup("group","Generic");
-        chris.createGroup("test","Generic");
+        chris.createGroup("group","ACCOUNTING");
+        chris.createGroup("test","ACCOUNTING");
         tree.addUser(chris,"group");
         tree.addUser(demarco,"group");
-        tree.addUser(colton,"group");
+        tree.addUser(colten,"group");
         System.out.println(tree.toString());
         GenData group =tree.getGroup("group");
 
@@ -140,8 +162,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //-----------------------------------------------------------------------------------------------------------------------------------+
 //                Test for Queue                                                                                             |
 //-----------------------------------------------------------------------------------------------------------------------------------+
-//Code Review//
-        //Was the Queue List Implmentation the best thing to use //
+
         Message a = new Message("test","this is a test meassg1e");
         Message b = new Message("test","this is a test meassge2");
 
@@ -170,6 +191,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+
 //-----------------------------------------------------------------------------------------------------------------------------------+
 //                Button Handler                                                                                                     |
 //-----------------------------------------------------------------------------------------------------------------------------------+
@@ -191,17 +213,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if (v == butCal) {
             startActivity(Calendar);
         }
-            else if (v == butFind){
-                updateList();
-            }
+        else if (v == butFind){
+            updateList();
+        }
+        else if(v == remOne){
+
+            tree.removeGroup(groups[0].getId());
+            user.removeGroup(groups[0].getId());
+            updateList();
+
+        }
+        else if(v == remTwo){
+            tree.removeGroup(groups[1].getId());
+            user.removeGroup(groups[1].getId());
+            updateList();
+
+        }
+        else if(v == remThree){
+            tree.removeGroup(groups[2].getId());
+            user.removeGroup(groups[2].getId());
+            updateList();
+        }
+        else if(v == remFour){
+            tree.removeGroup(groups[3].getId());
+            user.removeGroup(groups[3].getId());
+            updateList();
+        }
+        else if(v == remFive){
+            tree.removeGroup(groups[4].getId());
+            user.removeGroup(groups[4].getId());
+            updateList();
+        }
+
         }
     public void updateList(){
-        if(user.getGroups().getLength()!=0) groups=user.getGroups().toArr();
+        if(user.getCreatedGroups().getLength()!=0) groups=user.getCreatedGroups().toArr();
 
 
-        for(int x=0;x<groups.length&&x<5;x++){
+
+        for(int x=0;x<groups.length&&x<4;x++){
             groupList[x].setText(" ");
-            if(groups[x]!=null)groupList[x].setText(" "+groups[x].toString());
+            remove[x].setVisibility(View.INVISIBLE);
+            if(groups[x]!=null){
+                groupList[x].setText(" "+groups[x].toString());
+                remove[x].setVisibility(View.VISIBLE);
+
+            }
 
         }
     }
